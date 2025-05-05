a4ef5e043d0c2ffa4ac457912423d960c96e6561
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-05-05 13:37:30 +0700

    breaking the records

--- hackerrank.js
+++ hackerrank.js
@@ -156,3 +156,24 @@ function getTotalX(aa, bb) {
   return;
 }
 // getTotalX(aa, bb);
+
+// breaking the records;
+let scores = [10, 5, 20, 20, 4, 5, 2, 25, 1];
+function breakingRecords(scores) {
+  let min = scores[0];
+  let max = scores[0];
+  let minCount = 0;
+  let maxCount = 0;
+
+  for (let i = 1; i < scores.length; i++) {
+    if (scores[i] > max) {
+      max = scores[i];
+      maxCount++;
+    } else if (scores[i] < min) {
+      min = scores[i];
+      minCount++;
+    }
+  }
+  console.log(maxCount, minCount);
+}
+breakingRecords(scores);
