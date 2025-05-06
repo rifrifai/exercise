324057b74bc0e6cdb7308be5d2c14c3b11033c00
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-05-06 09:10:23 +0700

    initial commit

---
+++ coba.cs
@@ -0,0 +1,19 @@
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+    public Person(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}, Age: {Age}";
+    }
+}
+
